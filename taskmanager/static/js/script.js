06407d7ae67acb907aa1e8eefbb911d97e19e131
zimmoc <128923890+zimmoc@users.noreply.github.com>
2024-03-17 23:51:23 +0100

    Add form and functionality to add task

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,4 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
+  // sidenav init
   let sidenav = document.querySelectorAll('.sidenav');
   M.Sidenav.init(sidenav);
+
+  // datepicker init
+  let datepicker = document.querySelectorAll('.datepicker');
+  M.Datepicker.init(datepicker, {
+    format: "dd mmmm, yyyy",
+    i18n: {done: "Select"}
+  });
+
+  // select init
+ let selects = document.querySelectorAll('select');
+  M.FormSelect.init(selects);
 });
